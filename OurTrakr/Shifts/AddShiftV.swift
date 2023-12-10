aa31be73fbdad7ceefbd344a292ad431780fde22
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-10 02:01:09 -0500

    Display and calculate start and end times to shift

--- OurTrakr/Shifts/AddShiftV.swift
+++ OurTrakr/Shifts/AddShiftV.swift
@@ -9,27 +9,33 @@ import SwiftUI
 
 struct AddShiftV: View {
     @ObservedObject var job: FetchedResults<Job>.Element
-    @Environment(\.managedObjectContext) var moc
-    @Environment(\.dismiss) var dismiss
+    @Environment(\.managedObjectContext) private var moc
+    @Environment(\.dismiss) private var dismiss
+    @State var startTime: Date = Date.now.addingTimeInterval(-14400)
+    @State var endTime: Date = Date()
     
     var body: some View {
         NavigationStack {
-            Text("Placeholder")
-                .navigationTitle("New Shift")
-                .toolbar {
-                    ToolbarItem(placement: .cancellationAction) {
-                        Button("Cancel") {
-                            dismiss()
-                        }
-                    }
-                    ToolbarItem(placement: .confirmationAction) {
-                        Button("Add") {
-                            let newShift = Shift(context: moc)
-                            newShift.job = job
-                            dismiss()
-                        }
+            Form {
+                Section("Shift Times") {
+                    DatePicker("Start time", selection: $startTime)
+                    DatePicker("End time", selection: $endTime)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss()
                     }
                 }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Add") {
+                        let newShift = Shift(context: moc)
+                        newShift.job = job
+                        dismiss()
+                    }
+                }
+            }
         }
     }
 }
