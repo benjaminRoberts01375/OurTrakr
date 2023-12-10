3e6a052811c9d7fa83a2b7ebdb4a727f8e860045
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-09 23:56:07 -0500

    Initial project w/ SwiftLint

---
+++ OurTrakr/OurTrakrApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurTrakrApp.swift
+//  OurTrakr
+//
+//  Created by Ben Roberts on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct OurTrakrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
