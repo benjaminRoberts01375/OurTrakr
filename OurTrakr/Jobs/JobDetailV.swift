bc2dce5b5e3f5b627c34229699ca5343ccb7f409
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-10 01:34:26 -0500

    Added a sprinkle of formatting

--- OurTrakr/Jobs/JobDetailV.swift
+++ OurTrakr/Jobs/JobDetailV.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct JobDetailV: View {
     @ObservedObject var job: FetchedResults<Job>.Element
+    @Environment(\.managedObjectContext) var moc
     
     var body: some View {
-        Text(job.name ?? "No name")
+        List(Array(job.unwrappedShifts)) { shift in
+            Text("\(shift.start ?? Date())")
+        }
+        .navigationTitle("\(job.unwrappedName) Shifts")
+        .toolbar {
+            Button("Add", systemImage: "plus") {
+                let shift = Shift(context: moc)
+                shift.job = job
+                shift.start = Date() // Temp
+                shift.end = Date() // Temp
+            }
+        }
     }
 }
