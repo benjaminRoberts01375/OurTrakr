57165d1ce73e2b0ae070a93201f528c02a8e06d5
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-10 01:36:10 -0500

    Added AddShiftV

---
+++ OurTrakr/Shifts/AddShiftV.swift
@@ -0,0 +1,14 @@
+//
+//  AddShiftV.swift
+//  OurTrakr
+//
+//  Created by Ben Roberts on 12/10/23.
+//
+
+import SwiftUI
+
+struct AddShiftV: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
