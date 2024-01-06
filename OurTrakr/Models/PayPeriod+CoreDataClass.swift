6637fea2827ef862ea0c8e33d0770d338049f010
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-06 17:06:45 -0500

    Conform PayPeriod to Identifiable

--- OurTrakr/Models/PayPeriod+CoreDataClass.swift
+++ OurTrakr/Models/PayPeriod+CoreDataClass.swift
@@ -10,6 +10,6 @@ import Foundation
 import CoreData
 
 @objc(PayPeriod)
-public class PayPeriod: NSManagedObject {
+public class PayPeriod: NSManagedObject, Identifiable {
     var unwrappedShifts: Set<Shift> { self.shift as? Set<Shift> ?? [] }
 }
